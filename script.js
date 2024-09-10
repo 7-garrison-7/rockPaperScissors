09dd773052c09a8a5115a3944c3104594e2eeba8
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-10 00:33:17 -0400

    Create function to return random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand <= (1/3)) {
+        return "rock";
+    } else if (rand <= (2/3)) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// console.log(getComputerChoice());
