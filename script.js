93f0318560b8b6033d3ed25bce4c626d6e5b2085
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-25 01:20:04 -0400

    Declare winners once 5pts is reached on either player

--- script.js
+++ script.js
@@ -40,30 +40,49 @@ scissorsBut.addEventListener('click', () => {
         
         let message;
 
-        if (humanChoice == "paper" && computerChoice == "rock") {
-            humanScore++;
-            message = "You win! Paper beats rock";
-        } else if (humanChoice == "rock" && computerChoice == "scissors") {
-            message = "You win! Rock beats scissors";
-            humanScore++;
-        } else if (humanChoice == "scissors" && computerChoice == "paper") {
-            message = "You win! Scissors beat paper";
-            humanScore++;
-        } else if (computerChoice == "paper" && humanChoice == "rock") {
-            message = "You lose! Paper beats rock";
-            computerScore++;
-        } else if (computerChoice == "rock" && humanChoice == "scissors") {
-            message = "You lose! Rock beats scissors";
-            computerScore++;
-        } else if (computerChoice == "scissors" && humanChoice == "paper") {
-            message = "You lose! Scissors beat paper";
-            computerScore++;
-        } else if (computerChoice == humanChoice) {
-            message = "Tie!";
+        if (humanScore < 5 && computerScore < 5) {
+            if (humanChoice == "paper" && computerChoice == "rock") {
+                humanScore++;
+                message = "You win! Paper beats rock";
+            } else if (humanChoice == "rock" && computerChoice == "scissors") {
+                message = "You win! Rock beats scissors";
+                humanScore++;
+            } else if (humanChoice == "scissors" && computerChoice == "paper") {
+                message = "You win! Scissors beat paper";
+                humanScore++;
+            } else if (computerChoice == "paper" && humanChoice == "rock") {
+                message = "You lose! Paper beats rock";
+                computerScore++;
+            } else if (computerChoice == "rock" && humanChoice == "scissors") {
+                message = "You lose! Rock beats scissors";
+                computerScore++;
+            } else if (computerChoice == "scissors" && humanChoice == "paper") {
+                message = "You lose! Scissors beat paper";
+                computerScore++;
+            } else if (computerChoice == humanChoice) {
+                message = "Tie!";
+            }
+
+            results.textContent = message + "\nYour score: " + humanScore + "\nComputer score: " + computerScore;
+        } else if (humanScore >= 5) {
+            results.textContent = "You win the game!";
+        } else {
+            results.textContent = "You lose the game!";
         }
 
-        let postRoundMsg = message + "\nYour score: " + humanScore + "\nComputer score: " + computerScore;
-        results.textContent = postRoundMsg;
+        // let winnerDecided = false;
+
+        // if (humanScore >= 5 && !winnerDecided ) {
+        //     results.textContent = "You win the game!";
+        //     winnerDecided = true;
+        // } else if (computerScore >= 5 && !winnerDecided) {
+        //     
+        //     winnerDecided = true;
+        // } else {
+        //     
+        // }
+
+        // results.textContent = postRoundMsg;
     }
     
 
